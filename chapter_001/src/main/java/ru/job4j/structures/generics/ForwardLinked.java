4d5159995fca8e01e5d6350828fec5a86a6d2c98
Kirill Romanov <kirill.coldline@gmail.com>
2020-05-05 19:52:26 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#277460]

--- chapter_001/src/main/java/ru/job4j/structures/generics/ForwardLinked.java
+++ chapter_001/src/main/java/ru/job4j/structures/generics/ForwardLinked.java
@@ -2,21 +2,41 @@ package ru.job4j.structures.generics;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
 
+    private Node<T> tail;
+
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
     public void add(T value) {
-        Node<T> node = new Node<T>(value, null);
+        var newNode = new Node<T>(value);
         if (head == null) {
-            head = node;
-            return;
+            head = newNode;
+        } else if (tail == null) {
+            tail = newNode;
+            head.next = tail;
+            tail.prev = head;
+        } else {
+            tail.next = newNode;
+            var old = tail;
+            tail = newNode;
+            newNode.prev = old;
         }
-        Node<T> tail = head;
-        while (tail.next != null) {
-            tail = tail.next;
+    }
+
+    public T get(int index) {
+        Node<T> result = this.head;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
         }
-        tail.next = node;
+        return result.value;
     }
 
     public void deleteFirst() {
@@ -25,6 +45,17 @@ public class ForwardLinked<T> implements Iterable<T> {
         } else {
             throw new NoSuchElementException();
         }
+    }
+
+    public T deleteLast() {
+        T result = null;
+        if (iterator().hasNext()) {
+            result = tail.value;
+            tail = tail.prev;
+        } else {
+            throw new NoSuchElementException();
+        }
+        return result;
     }
 
     @Override
@@ -52,10 +83,10 @@ public class ForwardLinked<T> implements Iterable<T> {
     private static class Node<T> {
         T value;
         Node<T> next;
+        Node<T> prev;
 
-        public Node(T value, Node<T> next) {
+        public Node(T value) {
             this.value = value;
-            this.next = next;
         }
     }
 }
---
+++ chapter_001/src/main/java/ru/job4j/structures/generics/SimpleStack.java
@@ -0,0 +1,15 @@
+package ru.job4j.structures.generics;
+
+import java.util.NoSuchElementException;
+
+public class SimpleStack<T> {
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    public T pop() {
+        return linked.deleteLast();
+    }
+
+    public void push(T value) {
+        linked.add(value);
+    }
+}
