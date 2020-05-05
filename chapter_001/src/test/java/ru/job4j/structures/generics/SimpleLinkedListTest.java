a906cf805193e25104dab15f9a2d191b40b93f66
Kirill Romanov <kirill.coldline@gmail.com>
2020-05-05 18:04:50 +0300

    2. Создать контейнер на базе связанного списка [#277462]

--- chapter_001/src/test/java/ru/job4j/structures/generics/SimpleLinkedListTest.java
+++ chapter_001/src/test/java/ru/job4j/structures/generics/SimpleLinkedListTest.java
@@ -13,7 +13,7 @@ public class SimpleLinkedListTest {
 
     @Test
     public void whenAddThenGet() {
-        SimpleArrayList<String> array = new SimpleArrayList<>();
+        SimpleLinkedList<String> array = new SimpleLinkedList<>();
         array.add("first");
         String rsl = array.get(0);
         assertThat(rsl, is("first"));
@@ -21,7 +21,7 @@ public class SimpleLinkedListTest {
 
     @Test
     public void whenAddThenIt() {
-        SimpleArrayList<String> array = new SimpleArrayList<>();
+        SimpleLinkedList<String> array = new SimpleLinkedList<>();
         array.add("first");
         String rsl = array.iterator().next();
         assertThat(rsl, is("first"));
@@ -29,26 +29,26 @@ public class SimpleLinkedListTest {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenGetEmpty() {
-        SimpleArrayList<String> array = new SimpleArrayList<>();
+        SimpleLinkedList<String> array = new SimpleLinkedList<>();
         array.get(0);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenGetOutBound() {
-        SimpleArrayList<String> array = new SimpleArrayList<>();
+        SimpleLinkedList<String> array = new SimpleLinkedList<>();
         array.add("first");
         array.get(1);
     }
 
     @Test(expected = NoSuchElementException.class)
     public void whenGetEmptyFromIt() {
-        SimpleArrayList<String> array = new SimpleArrayList<>();
+        SimpleLinkedList<String> array = new SimpleLinkedList<>();
         array.iterator().next();
     }
 
     @Test(expected = ConcurrentModificationException.class)
     public void whenCorruptedIt() {
-        SimpleArrayList<String> array = new SimpleArrayList<>();
+        SimpleLinkedList<String> array = new SimpleLinkedList<>();
         array.add("first");
         Iterator<String> it = array.iterator();
         array.add("second");
