2e06a41b46e7b1f706056cb6017a7e1d236391e5
Kirill Romanov <kirill.coldline@gmail.com>
2020-06-10 22:44:15 +0300

    2. Статистику по коллекции. [#277455]

---
+++ chapter_001/src/main/java/ru/job4j/structures/exam/Analize.java
@@ -0,0 +1,43 @@
+package ru.job4j.structures.exam;
+
+import java.util.List;
+
+public class Analize {
+
+    public Info diff(List<User> previous, List<User> current) {
+        Info info = new Info();
+        if ((current.size() - previous.size() > 0)) {
+            info.added = current.size() - previous.size();
+        } else {
+            info.deleted = previous.size() - current.size();
+        }
+        for (var prevEl : previous) {
+            for (var curEl : current) {
+                if (prevEl.id == curEl.id && !prevEl.name.equals(curEl.name)) {
+                    info.changed++;
+                }
+                if (previous.indexOf(prevEl) >= current.indexOf(curEl)) {
+                    break;
+                }
+            }
+        }
+        return info;
+    }
+
+    public static class User {
+        int id;
+        String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    public static class Info {
+        int added;
+        int changed;
+        int deleted;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/structures/exam/AnalizeTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.structures.exam;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class AnalizeTest {
+    @Test
+    public void whenDelete() {
+        Analize analize = new Analize();
+        List<Analize.User> prev = new ArrayList<>();
+        List<Analize.User> cur = new ArrayList<>();
+        prev.add(new Analize.User(1, "first"));
+        prev.add(new Analize.User(2, "second"));
+        prev.add(new Analize.User(3, "third"));
+        cur.add(new Analize.User(2, "second"));
+        int deleted = analize.diff(prev, cur).deleted;
+        assertThat(deleted, is(2));
+    }
+
+    @Test
+    public void whenAdd() {
+        Analize analize = new Analize();
+        List<Analize.User> prev = new ArrayList<>();
+        prev.add(new Analize.User(1, "first"));
+        prev.add(new Analize.User(2, "second"));
+        prev.add(new Analize.User(3, "third"));
+        List<Analize.User> cur = new ArrayList<>(prev);
+        cur.add(new Analize.User(4, "fourth"));
+        int added = analize.diff(prev, cur).added;
+        assertThat(added, is(1));
+    }
+
+    @Test
+    public void whenAddThenChange() {
+        Analize analize = new Analize();
+        List<Analize.User> prev = new ArrayList<>();
+        List<Analize.User> cur = new ArrayList<>();
+        prev.add(new Analize.User(1, "first"));
+        prev.add(new Analize.User(2, "second"));
+        cur.add(new Analize.User(1, "first"));
+        cur.add(new Analize.User(2, "second"));
+        cur.get(0).name = "firstNew";
+        int changed = analize.diff(prev, cur).changed;
+        assertThat(changed, is(1));
+    }
+}
