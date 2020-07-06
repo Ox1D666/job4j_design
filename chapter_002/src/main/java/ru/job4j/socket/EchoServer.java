4cbb711757435fd868694d91ee9bae442c8c17db
Kirill Romanov <kirill.coldline@gmail.com>
2020-07-06 13:30:11 +0300

    1. Бот [#277523]

--- chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 
 public class EchoServer {
     private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+    private static final String HELLO = "Hello";
+    private static final String EXIT = "Exit";
 
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -18,18 +20,18 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    do {
-                        str = in.readLine();
-                        System.out.println(str);
-                        if (str.contains("Exit")) {
+                    String str = in.readLine();
+                    if (str != null) {
+                        String msg = str.substring(str.lastIndexOf("=") + 1, str.lastIndexOf(' '));
+                        if (msg.equals(HELLO)) {
+                            System.out.println("Hello, dear friend");
+                        } else if (msg.equals(EXIT)) {
+                            server.close();
                             stopServer = true;
-                        } else if (str.contains("Hello")) {
-                            System.out.println("Hello, dear friend.");
                         } else {
                             System.out.println(str);
                         }
-                    } while (!str.isEmpty());
+                    }
                     out.write("HTTP/1.1 200 OK\r\r\r\n\\".getBytes());
                 } catch (IOException e) {
                     LOG.error("I don't know what exception must be hear, beside fileNotFound", e);
