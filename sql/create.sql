19e6c3785993e49abe6eab34443519f208cb58e9
Kirill Romanov <kirill.coldline@gmail.com>
2020-07-16 13:00:07 +0300

    1. Схема прав пользователи и роли. [#277441]

---
+++ sql/create.sql
@@ -0,0 +1,45 @@
+/*CREATE TABLE Users (
+    UserID serial primary key,
+    login text,
+    passord text	
+);
+
+create table Roles (
+	RoleDesc varchar(50)
+);
+
+create table Rules (
+	RoleDescription text
+);
+
+create table Comments (
+	Description text
+);
+
+create table Attachs (
+	Files text
+);
+
+create table Сategories (
+	Category text
+);
+
+create table States (
+	State text
+);
+*/
+/*create table Item (
+	ItemID serial primary key,
+	ItemDescription text references Comments(Description),
+	ItemAttachs text references Attachs(Files),
+	ItemCategory text references Сategories(Category),
+	ItemState text references States(State)
+);*/
+/*alter table Сategories
+add primary key (Category);*/ 
+-- repeat for Comments, Attachs, States & Rules
+
+
+
+
+
