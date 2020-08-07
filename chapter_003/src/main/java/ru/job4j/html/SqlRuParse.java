8a17e92c407a30e318f37ffe0511b71efe1a7e6c
Kirill Romanov <kirill.coldline@gmail.com>
2020-08-07 14:27:25 +0300

    2. Парсинг HTML страницы. [#277436]

--- chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
+++ chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,7 +8,7 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
+        Elements row = doc.selectFirst(".forumTable").select(".postslisttopic");
         for (Element td : row) {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
