0f7bc479384ef69117b9cec73643dda70e9d38a7
Kirill Romanov <kirill.coldline@gmail.com>
2020-08-10 19:03:53 +0300

    2.1. Преобразование даты [#340968]

--- chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
+++ chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,15 +5,28 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
+import java.util.Locale;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/1").get();
+        Locale ru = new Locale("ru");
+        DateFormatSymbols instance = DateFormatSymbols.getInstance(ru);
+        instance.setShortMonths(new String[]{"янв", "февр", "мар", "апр", "мая", "июн", "июл", "авг", "сент", "окт", "нояб", "дек"});
+        SimpleDateFormat format = new SimpleDateFormat("dd MMM yy, HH:mm", ru);
+        format.setDateFormatSymbols(instance);
         Elements row = doc.selectFirst(".forumTable").select(".postslisttopic");
         for (Element td : row) {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
-            System.out.println(td.lastElementSibling().text());
+            System.out.println(format.parse(td.lastElementSibling().text()));
         }
     }
 }
