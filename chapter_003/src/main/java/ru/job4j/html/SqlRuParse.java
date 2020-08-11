08cd86982adb42b4014f89aaba44d26942e8577d
Kirill Romanov <kirill.coldline@gmail.com>
2020-08-11 14:05:50 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#340964]

--- chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
+++ chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,38 +4,43 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/1").get();
-        Locale ru = new Locale("ru");
-        DateFormatSymbols instance = DateFormatSymbols.getInstance(ru);
-        instance.setShortMonths(new String[]{"янв", "февр", "мар", "апр", "мая", "июн", "июл", "авг", "сент", "окт", "нояб", "дек"});
-        SimpleDateFormat dateFormatALL = new SimpleDateFormat("dd MMM yy, HH:mm", ru);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yy", ru);
-        dateFormatALL.setDateFormatSymbols(instance);
-        dateFormat.setDateFormatSymbols(instance);
-        Calendar cal = Calendar.getInstance();
-        Elements row = doc.selectFirst(".forumTable").select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            String date = td.lastElementSibling().text();
-            if (date.contains("сегодня")) {
-                String time = date.split(" ")[1];
-                System.out.println(dateFormat.format(cal.getTime()) + ", " + time);
-            } else if (date.contains("вчера")) {
-                String time = date.split(" ")[1];
-                cal.add(Calendar.DATE, -1);
-                System.out.println(dateFormat.format(cal.getTime()) + ", " + time);
-            } else {
-                System.out.println(dateFormatALL.format(dateFormatALL.parse(td.lastElementSibling().text())));
+        String[] url = "https://www.sql.ru/forum/job-offers/1".split("");
+        int page = Integer.parseInt(url[url.length - 1]);
+        for (; page <= 5; page++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
+            System.out.println(page + "---------------------------------------------------------------------------------------------------------------------------");
+            Locale ru = new Locale("ru");
+            DateFormatSymbols instance = DateFormatSymbols.getInstance(ru);
+            instance.setShortMonths(new String[]{"янв", "февр", "мар", "апр", "мая", "июн", "июл", "авг", "сент", "окт", "нояб", "дек"});
+            SimpleDateFormat dateFormatALL = new SimpleDateFormat("dd MMM yy, HH:mm", ru);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yy", ru);
+            dateFormatALL.setDateFormatSymbols(instance);
+            dateFormat.setDateFormatSymbols(instance);
+            Calendar cal = Calendar.getInstance();
+            Elements row = doc.selectFirst(".forumTable").select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                String date = td.lastElementSibling().text();
+                if (date.contains("сегодня")) {
+                    String time = date.split(" ")[1];
+                    System.out.println(dateFormat.format(cal.getTime()) + ", " + time);
+                } else if (date.contains("вчера")) {
+                    String time = date.split(" ")[1];
+                    cal.add(Calendar.DATE, -1);
+                    System.out.println(dateFormat.format(cal.getTime()) + ", " + time);
+                } else {
+                    System.out.println(dateFormatALL.format(dateFormatALL.parse(td.lastElementSibling().text())));
+                }
             }
         }
     }
