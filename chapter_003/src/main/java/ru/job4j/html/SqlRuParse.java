9752c07820fd61a3c66a1aacf0d9f83570854771
Kirill Romanov <kirill.coldline@gmail.com>
2020-08-11 13:40:42 +0300

    2.1. Преобразование даты [#340968]

--- chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
+++ chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,12 +4,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -19,14 +16,27 @@ public class SqlRuParse {
         Locale ru = new Locale("ru");
         DateFormatSymbols instance = DateFormatSymbols.getInstance(ru);
         instance.setShortMonths(new String[]{"янв", "февр", "мар", "апр", "мая", "июн", "июл", "авг", "сент", "окт", "нояб", "дек"});
-        SimpleDateFormat format = new SimpleDateFormat("dd MMM yy, HH:mm", ru);
-        format.setDateFormatSymbols(instance);
+        SimpleDateFormat dateFormatALL = new SimpleDateFormat("dd MMM yy, HH:mm", ru);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yy", ru);
+        dateFormatALL.setDateFormatSymbols(instance);
+        dateFormat.setDateFormatSymbols(instance);
+        Calendar cal = Calendar.getInstance();
         Elements row = doc.selectFirst(".forumTable").select(".postslisttopic");
         for (Element td : row) {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
-            System.out.println(format.parse(td.lastElementSibling().text()));
+            String date = td.lastElementSibling().text();
+            if (date.contains("сегодня")) {
+                String time = date.split(" ")[1];
+                System.out.println(dateFormat.format(cal.getTime()) + ", " + time);
+            } else if (date.contains("вчера")) {
+                String time = date.split(" ")[1];
+                cal.add(Calendar.DATE, -1);
+                System.out.println(dateFormat.format(cal.getTime()) + ", " + time);
+            } else {
+                System.out.println(dateFormatALL.format(dateFormatALL.parse(td.lastElementSibling().text())));
+            }
         }
     }
 }
